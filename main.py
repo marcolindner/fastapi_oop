e92ad0c40de6972074b75ffd0f87f2a03644cdce
Marco Lindner <marco.lindner@gmx.org>
2023-10-13 14:31:11 +0200

    Implmenting mangum

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routes.root_route import router as root_router
 from routes.animals_route import router as animals_router
 from routes.animal_route import router as animal_router
 from routes.animal_details_route import router as animal_details_router
+from mangum import Mangum
 
 app = FastAPI(title="Animal API", version="0.0.1")
 
@@ -11,7 +12,4 @@ app.include_router(animals_router)
 app.include_router(animal_router)
 app.include_router(animal_details_router)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
+handler = Mangum(app)
