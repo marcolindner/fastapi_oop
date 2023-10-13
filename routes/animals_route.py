a514176e08e3ba328ae57218091a11ca0df1bd64
Marco Lindner <marco.lindner@gmx.org>
2023-10-13 14:20:38 +0200

    Cleanup Comment

--- routes/animals_route.py
+++ routes/animals_route.py
@@ -7,11 +7,9 @@ router = APIRouter()
 # Returned eine reduzierte Liste von animals (Name, Rasse, Einlieferungsdatum)
 @router.get("/animals")
 def get_animals():
-    # return [animal.id, animal.name, animal.species, animal.arrival_date]
     miniAnimalList = []
     
     for animal in animalsList:
         miniAnimalList.append({"id": animal.id,"name": animal.name, "species": animal.species, "arrival_date": animal.arrival_date})
     
     return miniAnimalList
-
