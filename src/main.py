1bc8b0a19f4a37e227784eadaf744274cd7839d7
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-10-17 17:18:44 +0200

    serve static html via /index.html

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+#from fastapi.responses import FileResponse
+#import os
+
 from routes.root_route import router as root_router
-
-
 from routes.badge_route import router as badge_router
 
 from routes.student_route import router as student_router
@@ -15,15 +17,21 @@ from mangum import Mangum
 
 app = FastAPI(title="Student API", version="0.0.1")
 
-app.include_router(root_router)
+#app.include_router(root_router)
 app.include_router(students_router)
 app.include_router(teacher_router)
 app.include_router(teachers_router)
 app.include_router(student_router)
-
 app.include_router(badge_router)
 app.include_router(student_details_router)
 
+
+# Unterverzeichnis static für HTML Dateien konfigurieren
+app.mount("/", StaticFiles(directory="static"), name="static")
+
+#@app.get("/", response_class=FileResponse)
+#async def read_root():
+#    return FileResponse("static/index.html")
 
 if __name__ == "__main__":
     import uvicorn
